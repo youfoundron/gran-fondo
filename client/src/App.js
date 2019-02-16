99b2d5b4fb045e1254a436562aec662a1a4e72f1
Chris Walker <chris@ckwalker.com>
2019-02-15 21:07:17 -0700

    move symlink

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { loading: true, drizzleState: null }
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    console.log('*** DRIZZLE IS DRIZZLING ***')
+
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+  
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+  
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Drizzle, generateStore } from "drizzle";
+import StringStore from "./contracts/StringStore.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const drizzleOptions = { contracts: [StringStore] };
+
+const drizzleStore = generateStore(drizzleOptions);
+const drizzle = new Drizzle(drizzleOptions, drizzleStore);
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
