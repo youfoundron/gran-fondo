fe341ec9fadb03c432a51666949fbd222a2cf84a
Ron Gierlach <rongierlach@gmail.com>
2019-02-16 12:11:11 -0800

    stub more routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from './components/Layout';
 import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import User from './pages/User';
+import CreateChallengeSelectType from './pages/CreateChallengeSelectType';
+import CreateChallengeForm from './pages/CreateChallengeForm';
+import ChallengeDetails from './pages/ChallengeDetails';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +18,10 @@ class App extends Component {
           <Switch>
             <Route path='/login' exact={true} component={Login} />
             <PrivateRoute path='/' exact={true} component={Home} />
+            <PrivateRoute path='/user' component={User} />
+            <PrivateRoute path='/create' component={CreateChallengeSelectType} />
+            <PrivateRoute path='/create/:type(segment|distance)' component={CreateChallengeForm} />
+            <PrivateRoute path='/:type(segment|distance)/:challengeId' component={ChallengeDetails} />
           </Switch>
         </Layout>
       </Router>
